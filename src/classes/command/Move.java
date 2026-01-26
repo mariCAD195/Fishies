7e57ba992b62e1985e4f4bc936db7be6c2d96f29
mourek199 <antoninfranek199@gmail.com>
2026-01-26 12:38:59 +0100

    fixed what i thought already worked

--- src/classes/command/Move.java
+++ src/classes/command/Move.java
@@ -31,7 +31,6 @@ public class Move extends Command {
             if(map.getLocations().get(larry.getCurrentLocation().getName().toLowerCase()).getNeighbours().contains(input)){
                 larry.setCurrentLocation(map.getLocations().get(input));
                 System.out.println( "great choice! I'll be right there");
-                Toolbox.enter();
                 System.out.println(Toolbox.clearConsole());
                 System.out.println(Toolbox.coloredText("yellow",larry.getCurrentLocation().getName().toUpperCase()));
                 return larry.getCurrentLocation().getDescription();
