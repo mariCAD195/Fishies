653f35ab25e7112b6a344425b399537aa3e70944
mari <tumovamariana@gmail.com>
2026-01-29 15:27:43 +0100

    help command

--- src/classes/command/Help.java
+++ src/classes/command/Help.java
@@ -1,9 +1,17 @@
 package classes.command;
 
+import classes.Toolbox;
+
 public class Help extends Command {
     @Override
     public String execute() {
-        return "";
+        return Toolbox.coloredText("blue", "\n\"look around\" - look for everything insteresting in the room\n"+
+                "\"interact with\" - interact with an object in the room\n"+
+                "\"talk to\" - talk to NPC in the room\n"+
+                "\"search\" - search closely a fish tank in the room\n"+
+                "\"go home\" - end the day and come back the next morning\n"+
+                "\"move\" - move to a different room\n"+
+                "\"open journal\" - open journal where you can look at your progress\n");
     }
 
     @Override
