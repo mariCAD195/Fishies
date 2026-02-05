69392b68c9f4a651d8a0a4025cf993d800105f24
mari <tumovamariana@gmail.com>
2026-02-05 23:52:39 +0100

    just wrote some descriptions

--- src/classes/command/Move.java
+++ src/classes/command/Move.java
@@ -32,12 +32,25 @@ public class Move extends Command{
         String input = scanner.nextLine();
         if (map.getLocations().containsKey(input)) {
             if(map.getLocations().get(larry.getCurrentLocation().getName().toLowerCase()).getNeighbours().contains(input)){
-                larry.setCurrentLocation(map.getLocations().get(input));
-                System.out.println( "\ngreat choice! I'll be right there");
-                Toolbox.enter();
-                System.out.println(Toolbox.clearConsole());
-                System.out.println(Toolbox.coloredText("yellow",larry.getCurrentLocation().getName().toUpperCase()));
-                return larry.getCurrentLocation().getDescription();
+                if(input.equalsIgnoreCase("staff area")){
+                    if(larry.getInventory().contains("key card")){
+                        larry.setCurrentLocation(map.getLocations().get(input));
+                        System.out.println( "\nyou excitedly pull out your key card and after a quiet beep open the door to the staff room");
+                        Toolbox.enter();
+                        System.out.println(Toolbox.clearConsole());
+                        System.out.println(Toolbox.coloredText("yellow",larry.getCurrentLocation().getName().toUpperCase()));
+                        return larry.getCurrentLocation().getDescription();
+                    }else{
+                        return "you sadly stare at the closed door and dream about all the cool fishies who are surely hidden behind them\nmaybe you could try talking about it with some staff members";
+                    }
+                }else{
+                    larry.setCurrentLocation(map.getLocations().get(input));
+                    System.out.println( "\ngreat choice! I'll be right there");
+                    Toolbox.enter();
+                    System.out.println(Toolbox.clearConsole());
+                    System.out.println(Toolbox.coloredText("yellow",larry.getCurrentLocation().getName().toUpperCase()));
+                    return larry.getCurrentLocation().getDescription();
+                }
             }else{
                 return "sure i would love to go there, but it's not possible from here";
             }
