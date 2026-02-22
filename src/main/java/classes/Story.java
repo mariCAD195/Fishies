7f3916cc1b3cdc5369569929ec7214e4628c0639
mari <tumovamariana@gmail.com>
2026-02-22 21:20:53 +0100

    GAME FINISHED, im calling it now i need a break :(

--- src/main/java/classes/Story.java
+++ src/main/java/classes/Story.java
@@ -42,9 +42,9 @@ public class Story {
      */
     public String introduction(Help help) {
         System.out.println("\nYou are Larry, a college student on summer break who thought this summer would be a perfect time to revisit his childhood dreams.");
-        Toolbox.wait(4000);
+        Toolbox.wait(2000);
         System.out.println("You bought a three month aquarium pass, packed your old polaroid camera, \nfound your old octopus themed tshirt (it’s a bit tight, but it will do) and headed straight to the local sea world.\n");
-        Toolbox.wait(6000);
+        Toolbox.wait(3000);
         System.out.println("It can’t be that hard to take a photo of every fish there, can it?\n");
         Toolbox.enter();
         return help.execute();
--- src/main/java/command/Search.java
+++ src/main/java/command/Search.java
@@ -28,56 +28,60 @@ public class Search extends Command {
      */
     @Override
     public String execute() {
-        System.out.println("\n what do you want to search?\n");
-        map.getLocations().get(larry.getCurrentLocation().getName().toLowerCase()).searchMenu();
-        System.out.println("\n(type aquarium name)");
-        String input = scanner.nextLine();
-        if (map.getLocations().get(larry.getCurrentLocation().getName().toLowerCase()).getAquariums().contains(input)) {
-            System.out.println(Toolbox.coloredText("blue", "\n1) look for fishies\n2) look at information sign"));
-            System.out.println("\n(type fishies/sign)\n");
-            String input2 = scanner.nextLine();
-            switch (input2) {
-                case "fishies":
-                    if(map.getAquariums().get(input.toLowerCase()).getActiveFish().size()==0){
-                        return "\noh...the tank looks empty, maybe come back later\nif you want to learn more about local fish habits, try reading informative signs or talk to staff to get more clues\n";
-                    }
-                    map.getAquariums().get(input.toLowerCase()).activeFishMenu();
-                    System.out.println("\ntype fish name if you want to take a photo of them");
-                    String input3 = scanner.nextLine().trim();
-                    boolean fishExists = false;
-                    for (int i = 0; i < map.getAquariums().get(input).getActiveFish().size(); i++) {
-                        if (fishExists == false) {
-                            if(map.getAquariums().get(input).getActiveFish().get(i).getSpecies().trim().equalsIgnoreCase(input3)) {
-                                journal.takePhoto(input3,story, larry);
-                                fishExists = true;
-                                return "";
+        if(!map.getLocations().get(larry.getCurrentLocation().getName().toLowerCase()).getAquariums().isEmpty()) {
+            System.out.println("\n what do you want to search?\n");
+            map.getLocations().get(larry.getCurrentLocation().getName().toLowerCase()).searchMenu();
+            System.out.println("\n(type aquarium name)");
+            String input = scanner.nextLine();
+            if (map.getLocations().get(larry.getCurrentLocation().getName().toLowerCase()).getAquariums().contains(input)) {
+                System.out.println(Toolbox.coloredText("blue", "\n1) look for fishies\n2) look at information sign"));
+                System.out.println("\n(type fishies/sign)\n");
+                String input2 = scanner.nextLine();
+                switch (input2) {
+                    case "fishies":
+                        if (map.getAquariums().get(input.toLowerCase()).getActiveFish().size() == 0) {
+                            return "\noh...the tank looks empty, maybe come back later\nif you want to learn more about local fish habits, try reading informative signs or talk to staff to get more clues\n";
+                        }
+                        map.getAquariums().get(input.toLowerCase()).activeFishMenu();
+                        System.out.println("\ntype fish name if you want to take a photo of them");
+                        String input3 = scanner.nextLine().trim();
+                        boolean fishExists = false;
+                        for (int i = 0; i < map.getAquariums().get(input).getActiveFish().size(); i++) {
+                            if (fishExists == false) {
+                                if (map.getAquariums().get(input).getActiveFish().get(i).getSpecies().trim().equalsIgnoreCase(input3)) {
+                                    journal.takePhoto(input3, story, larry);
+                                    fishExists = true;
+                                    return "";
+                                }
                             }
                         }
-                    }
-                    if (!fishExists) {
-                        return "sorry I don't see any fish like this";
-                    }
-                case "sign":
-                    map.getAquariums().get(input.toLowerCase()).everyFishSign();
-                    System.out.println("\ntype fish name if you want to learn more");
-                    String input4 = scanner.nextLine().trim();
-                    boolean fishExists2 = false;
-                    for (int i = 0; i < map.getAquariums().get(input).getEveryFish().size(); i++) {
-                        if (fishExists2 == false) {
-                            if(map.getAquariums().get(input).getEveryFish().get(i).getSpecies().trim().equalsIgnoreCase(input4)) {
-                                System.out.println("\n"+Toolbox.coloredText("yellow",journal.getAllFish().get(input4.toLowerCase()).getSpecies().toUpperCase()));
-                                System.out.println(journal.getAllFish().get(input4.toLowerCase()).getDescription());
-                                fishExists2 = true;
-                                return "";
+                        if (!fishExists) {
+                            return "sorry I don't see any fish like this";
+                        }
+                    case "sign":
+                        map.getAquariums().get(input.toLowerCase()).everyFishSign();
+                        System.out.println("\ntype fish name if you want to learn more");
+                        String input4 = scanner.nextLine().trim();
+                        boolean fishExists2 = false;
+                        for (int i = 0; i < map.getAquariums().get(input).getEveryFish().size(); i++) {
+                            if (fishExists2 == false) {
+                                if (map.getAquariums().get(input).getEveryFish().get(i).getSpecies().trim().equalsIgnoreCase(input4)) {
+                                    System.out.println("\n" + Toolbox.coloredText("yellow", journal.getAllFish().get(input4.toLowerCase()).getSpecies().toUpperCase()));
+                                    System.out.println(journal.getAllFish().get(input4.toLowerCase()).getDescription());
+                                    fishExists2 = true;
+                                    return "";
+                                }
                             }
                         }
-                    }
-                    if (!fishExists2) {
-                        return "sorry I don't see any fish like this";
-                    }
+                        if (!fishExists2) {
+                            return "\nsorry I don't see any fish like this";
+                        }
+                }
+            } else {
+                return "\nsorry, no fish tanks like that in here";
             }
         }else{
-            return "sorry, no fish tanks like that in here";
+            return "\nsorry, no fish tanks in here";
         }
         return "";
     }
